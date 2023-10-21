940d943a2ba2f0ce501cc2e76e5a7e0e1a8eae72
iqbalrafli13 <iqbal13rafli@gmail.com>
2023-10-21 22:11:29 +0700

    autocomplate google map

--- src/views/LocationView.vue
+++ src/views/LocationView.vue
@@ -5,16 +5,16 @@
             <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
                 <div class="bg-white px-4 py-5 sm:p-6">
                     <div>
-                        <input
+                        <GMapAutocomplete
                             placeholder="My destination"
-                            
+                            @place_changed="handleLocationChange"                            
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
-                        <!-- </input> -->
+                        </GMapAutocomplete>
                     </div>
                 </div>
                 <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
                     <button
-                        
+                        @click.prevent="handleSelectLocation"
                         type="button"
                         class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">
                         Find A Ride
@@ -27,7 +27,33 @@
 </template>
 <script setup>
 
+import { useLocationStore } from '@/stores/location.js'
+import { useRouter } from 'vue-router';
 
+const location = useLocationStore();
+const router = useRouter()
+
+const handleLocationChange = (e)=>{
+    console.log('handleLocation',e);
+    location.$patch({
+        destination:{
+            name:e.name,
+            address:e.formatted_address,
+            geometry:{
+              lat:e.geometry.location.lat(),
+              lng:e.geometry.location.lng(),
+            }
+        }
+    })
+}
+const handleSelectLocation = () => {
+
+    if (location.destination.name !== '') {
+        router.push({
+            name:'map'
+        })
+    }
+}
     
 </script>
 
