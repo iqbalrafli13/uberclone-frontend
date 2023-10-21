ea7e1f32e5a894baab5247f08c823e201971f28b
iqbalrafli13 <iqbal13rafli@gmail.com>
2023-10-21 20:44:46 +0700

    create location view and route location

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -9,6 +9,7 @@
                         Start Drivig
                     </button>
                     <button  
+                        @click="handleFindRide"
                         class="rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">
                         Find A ride
                     </button>
@@ -21,6 +22,15 @@
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router';
+
+
+    const router = useRouter()
+    const handleFindRide = () =>{
+        router.push({
+            name:'location'
+        })
+    }
 
 </script>
 
